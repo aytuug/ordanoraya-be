1f17023f989ab0f17ec915bb989ba2bd64b9047b
aytuug <aytug.akin99@outlook.com>
2024-04-08 17:21:41 +0300

    completed car-service development.

--- car-service/src/main/java/com/aytugakin/carservice/repository/CarFuelConsumptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aytugakin.carservice.repository;
-
-import com.aytugakin.carservice.entity.CarFuelConsumption;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarFuelConsumptionRepository extends JpaRepository<CarFuelConsumption, Long> {
-}
